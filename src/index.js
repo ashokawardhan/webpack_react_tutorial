f11a10d31754071d63f9034496e6edb69f3a12f9
Ashoka Chandra Wardhan <awardhan@expedia.com>
2018-07-22 10:54:05 +0530

    Style loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
+import './style.css';
+
 function component() {
     var element = document.createElement('div');
+    element.classList.add('hello');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');

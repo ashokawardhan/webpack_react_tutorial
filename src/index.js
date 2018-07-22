79c6345877428270c70a3950755b43a504f247fc
Ashoka Chandra Wardhan <awardhan@expedia.com>
2018-07-22 11:09:42 +0530

    Auto generate dist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from "./icon.png";
+import printMe from "./print.js";
 
 function component() {
-    var element = document.createElement('div');
-    element.classList.add('hello');
+    let element = document.createElement('div');
+    let btn = document.createElement('button');
+    btn.innerHTML = "Click me and check the console!";
+    btn.onclick = printMe;
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
+    element.innerHTML = "Hello webpack";
+    element.appendChild(btn);
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -22,5 +27,11 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ]
 };
